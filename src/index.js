86b791dc7aa083f3bd38e842b330d707e9efb0c4
spiffytech <spiffy@spiffy.tech>
2019-08-01 14:52:20 -0400

    Reproduced the Gun errors mentioned in Gitter chat

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello, world!');
+import Gun from 'gun';
+import 'gun/lib/rindexed';
+
+const gun = Gun({localStorage: false});
+window.Gun = Gun;
+window.gun = gun;
+
+const f = gun.get('foo1').put({id: 'foo1'});
+gun.get('foo-set').set(f);
+gun.get('foo-set').map().on(console.log);
